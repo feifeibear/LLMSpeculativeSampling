3505d006034226ebf67c5cf001ec1721043ff837
feifeibear <fangjiarui123@gmail.com>
2023-08-21 10:32:53 +0800

    use logits before sampling

--- sample.py
+++ sample.py
@@ -19,7 +19,7 @@ DECODER : Decoder = None
 def parse_arguments():
     parser = argparse.ArgumentParser(description='args for sample.py')
 
-    parser.add_argument('--input', type=str, default="A dog has four")
+    parser.add_argument('--input', type=str, default="Suggest at least five related search terms to \"Mạng neural nhân tạo\".")
     parser.add_argument('--approx_model_name', type=str, default="/share_nfs/fangjiarui/root/code/hf_models/bloom-560m")
     parser.add_argument('--target_model_name', type=str, default="/share_nfs/fangjiarui/root/code/hf_models/bloomz-7b1")
     parser.add_argument('--verbose', '-v', action='store_true', default=False, help='enable verbose mode')
@@ -43,11 +43,11 @@ def top_k_top_p_filter(logits, top_k: int = 0, top_p: float = 0.0):
     return logits
 
 
-def sample(logits : torch.Tensor, temperature : float, top_k : float, top_p : float) -> torch.Tensor:
-    """_summary_
+def norm_logits(logits : torch.Tensor, temperature : float, top_k : float, top_p : float) -> torch.Tensor:
+    """
 
     Args:
-        logits (torch.Tensor): shape (batch, vocab)
+        logits (torch.Tensor): shape (batch, seqlen, vocab)
         temperature (float): temperature
         top_k (float): top_k
         top_p (float): top_p
@@ -58,11 +58,14 @@ def sample(logits : torch.Tensor, temperature : float, top_k : float, top_p : fl
     logits = logits / temperature
     logits = top_k_top_p_filter(logits, top_k=top_k, top_p=top_p)
     probs = F.softmax(logits, dim=1)
-    idx_next = torch.multinomial(probs, num_samples=1)
+    return probs
+
+
+def sample(probs : torch.Tensor, num_samples: int = 1):
+    idx_next = torch.multinomial(probs, num_samples=num_samples)
     if (idx_next.item() == 0):
         raise RuntimeError
     return idx_next
-    
 
 def autoregressive_sampling(x : torch.Tensor, model : torch.nn.Module, N : int, 
                             temperature : float = 1, top_k : int = 0, top_p : float = 0):
@@ -72,7 +75,7 @@ def autoregressive_sampling(x : torch.Tensor, model : torch.nn.Module, N : int,
     with tqdm(total=N, desc="autoregressive sampling") as pbar:
         while n < T:
             logits = model(x).logits[::, -1, :]
-            idx_next = sample(logits, temperature, top_k, top_p)
+            idx_next = sample(norm_logits(logits, temperature, top_k, top_p))
             x = torch.cat((x, idx_next), dim=1)
             n += 1
             pbar.update(1)
@@ -89,11 +92,11 @@ def max_fn(x):
     x_max_sum = torch.sum(x_max, dim=1, keepdim=True) 
     return x_max / x_max_sum
 
-def norm_logits(p : torch.Tensor):
-    """
-        normalize logits using softmax to probabilities along the last dimension.
-    """
-    return F.softmax(p, dim=-1)
+# def norm_logits(p : torch.Tensor):
+#     """
+#         normalize logits using softmax to probabilities along the last dimension.
+#     """
+#     return F.softmax(p, dim=-1)
 
 def speculative_sampling_v2(prefix : torch.Tensor, approx_model : torch.nn.Module, target_model : torch.nn.Module, 
                          max_len : int , gamma : int = 4,
@@ -130,13 +133,19 @@ def speculative_sampling_v2(prefix : torch.Tensor, approx_model : torch.nn.Modul
             for _ in range(gamma):
                 # p.logits shape (batch, seq, vocab)
                 q = approx_model(x).logits
-                next_tok = sample(q[:, -1, :], 
-                                  temperature, top_k, top_p)
+                next_tok = sample(norm_logits(q[:, -1, :], 
+                                  temperature, top_k, top_p))
                 x = torch.cat((x, next_tok), dim=1)
 
-            q = norm_logits(q)
+            # normalize the logits
+            for i in range(q.shape[1]):
+                q[:,i,:] = norm_logits(q[:,i,:],
+                                temperature, top_k, top_p)
             # p  = M_p[prefix + x_0, x_0, .., x_(gamma-1)]
-            p = norm_logits(target_model(x).logits)
+            p = target_model(x).logits
+            for i in range(p.shape[1]):
+                p[:,i,:] = norm_logits(p[:,i,:],
+                                temperature, top_k, top_p)
 
             # n the end position of the valid prefix
             # x = x_[:prefix_len-1] + x_0, ... x_(gamma-1)
@@ -152,16 +161,14 @@ def speculative_sampling_v2(prefix : torch.Tensor, approx_model : torch.nn.Modul
                     n += 1
                 else:
                     # reject
-                    t = sample(max_fn(p[:, n, :] - q[:, n, :]), 
-                           temperature, top_k, top_p)
+                    t = sample(max_fn(p[:, n, :] - q[:, n, :]))
                     is_all_accept = False
                     break
          
             prefix = x[:, :n + 1]
             
             if is_all_accept:
-                t = sample(p[:, -1, :], 
-                           temperature, top_k, top_p)
+                t = sample(p[:, -1, :])
             
             prefix = torch.cat((prefix, t), dim=1)
             pbar.update(n - pbar.n)
@@ -206,13 +213,21 @@ def speculative_sampling(prefix : torch.Tensor, approx_model : torch.nn.Module, 
             for _ in range(gamma):
                 # p.logits shape (batch, seq, vocab)
                 q = approx_model(x).logits
-                next_tok = sample(q[:, -1, :], 
-                                  temperature, top_k, top_p)
+                next_tok = sample(norm_logits(q[:, -1, :], 
+                                  temperature, top_k, top_p))
                 x = torch.cat((x, next_tok), dim=1)
 
-            q = norm_logits(q)
+
+            # normalize the logits
+            for i in range(q.shape[1]):
+                q[:,i,:] = norm_logits(q[:,i,:],
+                                temperature, top_k, top_p)
+            
             # p  = M_p[prefix + x_0, x_0, .., x_(gamma-1)]
-            p = norm_logits(target_model(x).logits)
+            p = target_model(x).logits
+            for i in range(p.shape[1]):
+                p[:,i,:] = norm_logits(p[:,i,:],
+                                temperature, top_k, top_p)
 
             # n the end position of the valid prefix
             # x = x_[:prefix_len-1] + x_0, ... x_(gamma-1)
@@ -235,19 +250,18 @@ def speculative_sampling(prefix : torch.Tensor, approx_model : torch.nn.Module, 
             
             if n < prefix_len + gamma - 1:
                 # reject someone, sample from the pos n
-                t = sample(max_fn(p[:, n, :] - q[:, n, :]), 
-                           temperature, top_k, top_p)
+                t = sample(max_fn(p[:, n, :] - q[:, n, :]))
                 if verbose:
                     print(f"target resamples {n}: \033[34m{DECODER.decode(t)}\033[0m")
             else:
                 # all approx model decoding accepted
                 assert n == p.shape[1] - 1
-                t = sample(p[:, -1, :], 
-                           temperature, top_k, top_p)
+                t = sample(p[:, -1, :])
                 if verbose:
                     print(f"target samples {n}: \033[35m{DECODER.decode(t)}\033[0m")
                 
             prefix = torch.cat((prefix, t), dim=1)
+            
             if not verbose:
                 pbar.update(n - pbar.n)
 
@@ -268,25 +282,26 @@ def generate(input_text, approx_model_name, target_model_name, num_tokens=20, ve
 
     input_ids = tokenizer.encode(input_text, return_tensors='pt').to(torch_device)
 
-    
+    top_k = 10
+    top_p = 0.9
     torch.manual_seed(123)
-    output = speculative_sampling(input_ids, small_model, large_model, num_tokens, top_k = 10, verbose = verbose)
+    output = speculative_sampling(input_ids, small_model, large_model, num_tokens, top_k = top_k, top_p=top_p, verbose = verbose)
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     print(f"google's speculative_sampling: {generated_text}")
 
     # torch.manual_seed(123)
-    # output = speculative_sampling_v2(input_ids, small_model, large_model, num_tokens, top_k = 10)
+    # output = speculative_sampling_v2(input_ids, small_model, large_model, num_tokens, top_k = top_k, top_p=top_p, )
     # generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     # print(f"deepmind's speculative_sampling: {generated_text}")
 
 
-    # torch.manual_seed(123)
-    # output = autoregressive_sampling(input_ids, large_model, num_tokens, top_k = 10)
-    # generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-    # print(f"large (target) model autoregressive_sampling: {generated_text}")
+    torch.manual_seed(123)
+    output = autoregressive_sampling(input_ids, large_model, num_tokens, top_k = top_k, top_p=top_p)
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    print(f"large (target) model autoregressive_sampling: {generated_text}")
 
     # torch.manual_seed(123)
-    # output = autoregressive_sampling(input_ids, small_model, num_tokens, top_k = 10)
+    # output = autoregressive_sampling(input_ids, small_model, num_tokens, top_k = top_k, top_p=top_p)
     # generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     # print(f"small (approx) model autoregressive_sampling: {generated_text}")
 
